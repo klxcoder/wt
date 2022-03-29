2df83baf1482207435a58a344526f4d50c54d218
klxcoder <klxcoder@gmail.com>
2022-03-29 21:35:15 +0700

    add point and text ok

--- src/components/lineChart/LineChart.js
+++ src/components/lineChart/LineChart.js
@@ -13,24 +13,52 @@ function LineChart() {
     // update linechart every time the temperature changes
     useLayoutEffect(() => {
         const total = data.weekWeather.map(data => data.main.temp)
+        console.log(total);
+        console.log(data);
         setValue(total)
     }, [data])
 
+    function alternatePointRadius(ctx, nbr) {
+        const index = ctx.dataIndex;
+        return index === nbr ? 5 : 0;
+    }
+
+    const drawCurrentTemp = {
+        id: 'drawCurrentTemp',
+        afterDraw(chart, args, options) {
+            const { ctx } = chart;
+            const currentTemp = chart.getDatasetMeta(0)._dataset.currentTemp
+            const currentIndex = chart.getDatasetMeta(0)._dataset.currentIndex
+            ctx.save();
+            ctx.font = "20px Sans MS";
+            ctx.fillStyle = "green";
+            ctx.textAlign = "center";
+            if(chart.getDatasetMeta(0).data.length !=0) {
+                const {x, y} = chart.getDatasetMeta(0).data[currentIndex];
+                ctx.fillText(currentTemp, x, y - 10);
+            }
+            ctx.restore();
+        }
+    };
 
     return (
         <div>
             <Line
                 data={{
-                    labels: ['', '', '', '', '', '', '',],
+                    labels: ['first', 'second', '', '', '', '', '',],
                     datasets: [{
                         label: null,
                         data: value,
                         borderColor: '#70a0e8',
                         backgroundColor: '#e0e7f3',
-                        pointRadius: 0,
+                        pointRadius: (ctx) => alternatePointRadius(ctx, value ? value.indexOf(data.detalsWeather.temp * 1) : -1),
+                        pointBackgroundColor: '#70a0e8',
                         borderWidth: 2,
-                        tension: 0.4,
-                        fill: true,
+                        // tension: 0.4,
+                        pointStyle: 'circle',
+                        // fill: true,
+                        currentTemp: data.detalsWeather.temp,
+                        currentIndex: value ? value.indexOf(data.detalsWeather.temp * 1) : -1
                     }],
                 }}
                 height={200}
@@ -38,20 +66,31 @@ function LineChart() {
                     maintainAspectRatio: false,
                     scales: {
                         x: {
-                            display: false,
+                            display: true,
                         },
                         y: {
-                            min: -5,
-                            max: 35,
-                            display: false,
+                            // min: -5,
+                            // max: 35,
+                            display: true,
                         }
                     },
                     plugins: {
                         legend: {
                             display: false,
+                        },
+                        tooltip: {
+                            enabled: true,
+                        },
+                        drawCurrentTemp: {
+                            borderColor: 'red',
+                            borderWidth: 2,
+                            borderDash: [5, 5],
+                            borderDashOffset: 2,
+
                         }
                     },
                 }}
+                plugins={[drawCurrentTemp]}
             />
         </div>
     );
